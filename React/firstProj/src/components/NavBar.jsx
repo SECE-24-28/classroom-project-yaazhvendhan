5327d4ea9db6a6ea077e87dd82ea3fb3ccee8276
yaazhvendhan <yaazhvendhanms@gmail.com>
2025-12-09 22:54:59 +0530

    feat:Fixed css in login.html and index.css

--- React/firstProj/src/components/NavBar.jsx
+++ React/firstProj/src/components/NavBar.jsx
@@ -4,11 +4,10 @@ const NavBar = ()=>{
     return (
         <nav  className="nv">
         
-        <h5>home</h5>
-        <h5>about</h5>
-        <h5>order</h5>
-        <h5>review</h5>
-       
+        <a href="https://www.google.com" >Home</a>
+        <a href="https://www.google.com" >About</a>
+        <a href="https://www.google.com" >Reviews</a>
+        <a href="https://www.google.com" >Order</a>
         </nav>
 
     )
