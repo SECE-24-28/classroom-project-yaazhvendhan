18341b89e58dbd428d944aad9d9365df4f9092a7
yaazhvendhan <yaazhvendhanms@gmail.com>
2025-12-11 16:46:21 +0530

    updated feedBack app: implemented useState and useEffect and api.get in App.jsx

--- React/Day-9/feedBack/src/App.jsx
+++ React/Day-9/feedBack/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import api from './api/Post'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts,setPosts]=useState([])
+
+  useEffect(()=>
+  {
+    const fetData=async()=>{
+      const res = await api.get("/feedback");
+      setPosts(res.data)
+    }
+    fetData()
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {
+      posts.map((post)=>
+        <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.datetime}</p>
+        <p>{post.body}</p>
+        <hr />
+        </div>
+
+      
+      )
+    }
+  
     </>
   )
 }
