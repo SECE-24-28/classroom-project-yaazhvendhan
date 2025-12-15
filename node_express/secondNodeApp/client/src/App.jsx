445c8661cb9b4236ae56e1dbc1fca5c82d3cd089
yaazhvendhan <yaazhvendhanms@gmail.com>
2025-12-15 15:58:07 +0530

    Updated second node app : implemented app post functionality

--- node_express/secondNodeApp/client/src/App.jsx
+++ node_express/secondNodeApp/client/src/App.jsx
@@ -1,13 +1,62 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { addCourses, getCourses } from './api/CourseApi'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [courses, setCourses] = useState([])
+  const [title, setTitle] = useState("")
+  const [duration, setDuration] = useState("")
+
+  const fetchCourse = async () => {
+    const res = await getCourses()
+    setCourses(res.data)
+  }
+
+  useEffect(() => {
+    fetchCourse()
+  }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const course = { title, duration }
+    const res = await addCourses(course)
+
+    //  append the newly created course
+    setCourses(prevCourses => [...prevCourses, res.data])
+
+    setTitle("")
+    setDuration("")
+  }
 
   return (
     <>
-      <h1>Welcome</h1>
-      <hr />
+      <form className="frm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+
+        <input
+          type="text"
+          placeholder="duration"
+          value={duration}
+          onChange={(e) => setDuration(e.target.value)}
+          required
+        />
+
+        <button type="submit">Add</button>
+      </form>
+
+      <ul>
+        {courses.map(course => (
+          <li key={course._id}>
+            {course.title} - {course.duration}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
--- node_express/secondNodeApp/client/src/api/CourseApi.jsx
+++ node_express/secondNodeApp/client/src/api/CourseApi.jsx
@@ -1,1 +1,10 @@
-const API = axios.create({baseURL:"https://localhost:4000"})
+import axios from "axios";
+
+const api = axios.create({baseURL:"http://localhost:4000/api"})
+
+export const getCourses = ()=> api.get("/courses")
+export const addCourses = (course)=> api.post("/courses",course)
+export const updateCourses =(id,course)=> api.put(`/courses/${id}`,course)
+export const deleteCourses =(id)=> api.delete(`/courses/${id}`)
+
+
